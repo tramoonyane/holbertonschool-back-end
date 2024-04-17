1edc08bd837e2f831484c1168a56d10e7622b882
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-18 01:05:37 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,43 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
+"""Script using API and modume requests"""
+
+
 import requests
 import sys
 
 
+def fetch_todo_progress(employee_id):
+    """script API TODO list"""
+
+    employee_id = int(sys.argv[1])
+    # Endpoint URLs
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/\
+{employee_id}/todos"
+
+    # Fetch user information
+    user_response = requests.get(user_url)
+
+    user_data = user_response.json()
+    EMPLOYEE_NAME = user_data['name']
+
+    # Fetch TODOs for the user
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Calculate progress
+    TOTAL_NUMBER_OF_TASKS = len(todos_data)
+    NUMBER_OF_DONE_TASKS = sum(1 for TASK_TITLE in
+                               todos_data if TASK_TITLE['completed'])
+
+    # Print progress
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks\
+({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for TASK_TITLE in todos_data:
+        if TASK_TITLE['completed']:
+            print(f"\t {TASK_TITLE['title']}")
+
+
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
-
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
-
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+    employee_id = int(sys.argv[1])
+    fetch_todo_progress(employee_id)
