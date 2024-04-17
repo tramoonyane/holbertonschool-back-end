95cb027af175162ed0a180e0128ed029ca4235cc
tramoonyane <146661538+tramoonyane@users.noreply.github.com>
2024-04-17 13:26:44 +0200

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,28 +14,37 @@ def get_employee_todo_progress(employee_id):
 
     :param employee_id: An integer representing the employee ID.
     """
+ 
     base_url = 'https://jsonplaceholder.typicode.com/'
     user_url = f'{base_url}users/{employee_id}'
     user_response = requests.get(user_url)
+
     if user_response.status_code != 200:
         print(f"Error: Could not retrieve information for employee ID {employee_id}")
         return
+
     user_data = user_response.json()
     employee_name = user_data.get('name')
     employee_username = user_data.get('username')
+
     if employee_name is None:
         print(f"Error: Employee ID {employee_id} does not exist.")
         return
+
     todos_url = f'{base_url}todos?userId={employee_id}'
     todos_response = requests.get(todos_url)
+
     if todos_response.status_code != 200:
         print(f"Error: Could not retrieve TODO list for employee ID {employee_id}")
         return
+
     todos_data = todos_response.json()
     total_tasks = len(todos_data)
     done_tasks = [todo for todo in todos_data if todo.get('completed')]
     number_of_done_tasks = len(done_tasks)
+ 
     print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+
     for task in done_tasks:
         print(f"\t {task.get('title')}")
     filename = f'{employee_id}.csv'
